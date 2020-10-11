fbbf8cf0c935646e8c8a70b00aab628bf27ea3e0
weidong6686 <weidong6686@gmail.com>
2020-10-10 19:27:21 -0700

    javascript-assignment

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       // put data fetching code here!
+      const response = await fetch(
+        "http://demo1390455.mockable.io/articles"
+      );
+      const responseJson = await response.json();
+      console.log("Resonponse", responseJson);
+      setFetchedData(responseJson);
     };
 
     if (isEmpty(fetchedData)) {
